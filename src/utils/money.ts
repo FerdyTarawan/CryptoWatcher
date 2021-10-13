8b43e5e4638058f0bbe937821aad95b05d0920f5
FerdyTarawan <ferdy.tarawan@gmail.com>
2021-10-14 06:21:59 +0700

    bugfix: Fix price formatting

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -3,6 +3,5 @@ import type {Money} from "ts-money"
 export {Money, Currencies} from "ts-money"
 export const formatCurrency = (money: Money): string =>
   `${money.getCurrency()} ${money
-    .getAmount()
-    .toFixed(2)
-    .replace(/\d(?=(\d{3})+\.)/g, "$&,")}`
+    .toString()
+    .replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,")}`
